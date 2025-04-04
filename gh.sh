9d67ecfd3fc70bd83bdb750255d6ea00feeb8de2
Michael Popov <michaelpopov@Mac-mini-Michael.local>
2025-04-04 22:01:17 +0300

    gh.sh улучшил

--- gh.sh
+++ gh.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-[ -z "$1" ] && echo "Использование: $0 \"Сообщение коммита\"" && exit 1
 
-# Переходим в корень git-репозитория
-cd "$(git rev-parse --show-toplevel)" || exit 1
-
-# Сохраняем текущие изменения (включая untracked файлы)
-git stash push -u -m "Auto-stashed by gh.sh" 2>/dev/null
-
-# Подтягиваем изменения
-git pull --rebase || exit 1
-
-# Возвращаем изменения
-git stash pop 2>/dev/null
+git pull
 
 # Добавляем ВСЕ изменения
 git add -A . && \
